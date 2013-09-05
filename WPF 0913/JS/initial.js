d8e8d56be1223bd5e8ad8f2b1c6097545cc3ec7a
Jason Jones <jayjonesy1@fullsail.edu>
2013-09-04 23:52:34 -0400

    Adding Day 1 Assignment
    Day 1 Assignment, Flowchart plus class activity.

---
+++ WPF 0913/JS/initial.js
@@ -0,0 +1,53 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jayjonesy1
+ * Date: 9/4/13
+ * Time: 7:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//var initialValue= 10;
+//
+//
+//console.log(initialValue);
+//
+//initialValue =initialValue +1;
+//initialValue/=5;
+//console.log(initialValue);
+
+var len;
+var wid;
+var height;
+var area;
+var radius;
+
+
+len = 22;
+wid = 19;
+height = 13;
+radius=20;
+
+
+area = len*wid*height;
+circumfrence = radius*radius*Math.PI;
+
+
+console.log(area);
+
+console.log(Math.PI);
+
+console.log(circumfrence);
+
+var initialSalary;
+
+initialSalary=5000;
+
+console.log(initialSalary+ ' Year One');
+
+initialSalary=initialSalary+initialSalary*.05;
+
+console.log(initialSalary+ ' Year Two');
+
+initialSalary=initialSalary+initialSalary*.05;
+
+console.log(initialSalary+ ' Year Three');
+
